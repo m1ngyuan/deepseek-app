fd8b31cd3b067a0a865650c25727676be33ed6f7
Mingyuan Wu <my.wu@outlook.com>
2025-03-21 16:09:40 +0800

    *Support for Githhub Workflows

--- main.go
+++ main.go
@@ -4,53 +4,60 @@ import (
 	"context"
 	"github.com/openai/openai-go"
 	"github.com/openai/openai-go/option"
+	"log"
 	"os"
 )
 
 func main() {
-	apiKey, err := os.LookupEnv("DEEPSEEK_API_KEY")
-	if err {
-		panic(err)
+	if apiKey, ok := os.LookupEnv("DEEPSEEK_API_KEY"); ok {
+		baseURL := os.Getenv("DEEPSEEK_BASE_URL")
+		if baseURL == "" {
+			baseURL = "https://api.deepseek.com"
+		}
+		client := openai.NewClient(
+			option.WithAPIKey(apiKey), // defaults to os.LookupEnv("OPENAI_API_KEY")
+			option.WithBaseURL(baseURL),
+		)
+		stream := client.Chat.Completions.NewStreaming(context.TODO(), openai.ChatCompletionNewParams{
+			Messages: []openai.ChatCompletionMessageParamUnion{
+				openai.UserMessage("你如何评价小米ultra"),
+			},
+			Model: "deepseek-chat",
+		})
+		// optionally, an accumulator helper can be used
+		acc := openai.ChatCompletionAccumulator{}
+
+		for stream.Next() {
+			chunk := stream.Current()
+			acc.AddChunk(chunk)
+
+			if content, ok := acc.JustFinishedContent(); ok {
+				log.Println("Content stream finished:", content)
+			}
+
+			// if using tool calls
+			if tool, ok := acc.JustFinishedToolCall(); ok {
+				log.Println("Tool call stream finished:", tool.Index, tool.Name, tool.Arguments)
+			}
+
+			if refusal, ok := acc.JustFinishedRefusal(); ok {
+				log.Println("Refusal stream finished:", refusal)
+			}
+
+			// it's best to use chunks after handling JustFinished events
+			if len(chunk.Choices) > 0 {
+				log.Println(chunk.Choices[0].Delta.Content)
+			}
+		}
+		if err := stream.Err(); err != nil {
+			log.Fatalf("Stream error: %v", err)
+		}
+		finalContent := acc.Choices[0].Message.Content
+
+		log.Println(finalContent)
+	} else {
+
+		log.Fatal("environment variable DEEPSEEK_API_KEY is not set")
 	}
-	client := openai.NewClient(
-		option.WithAPIKey(apiKey), // defaults to os.LookupEnv("OPENAI_API_KEY")
-		option.WithBaseURL("https://api.deepseek.com"),
-	)
-	stream := client.Chat.Completions.NewStreaming(context.TODO(), openai.ChatCompletionNewParams{
-		Messages: []openai.ChatCompletionMessageParamUnion{
-			openai.UserMessage("你如何评价小米ultra"),
-		},
-		Model: "deepseek-chat",
-	})
-	// optionally, an accumulator helper can be used
-	acc := openai.ChatCompletionAccumulator{}
 
-	for stream.Next() {
-		chunk := stream.Current()
-		acc.AddChunk(chunk)
-
-		if content, ok := acc.JustFinishedContent(); ok {
-			println("Content stream finished:", content)
-		}
-
-		// if using tool calls
-		if tool, ok := acc.JustFinishedToolCall(); ok {
-			println("Tool call stream finished:", tool.Index, tool.Name, tool.Arguments)
-		}
-
-		if refusal, ok := acc.JustFinishedRefusal(); ok {
-			println("Refusal stream finished:", refusal)
-		}
-
-		// it's best to use chunks after handling JustFinished events
-		if len(chunk.Choices) > 0 {
-			println(chunk.Choices[0].Delta.Content)
-		}
-	}
-	if err := stream.Err(); err != nil {
-		panic(err)
-	}
-	refusal := acc.Choices[0].Message.Content
-
-	println(refusal)
 }
