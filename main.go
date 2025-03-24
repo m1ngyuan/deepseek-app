8ddbc945254748784992c4bc57a2282487b3d689
Mingyuan Wu <my.wu@outlook.com>
2025-03-24 13:47:18 +0800

    Gin

--- main.go
+++ main.go
@@ -6,9 +6,20 @@ import (
 	"github.com/openai/openai-go/option"
 	"log"
 	"os"
+	"runtime"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	ConfigRuntime()
+	StartWorkers()
+	StartGin()
+
+}
+
+func c() {
 	if apiKey, ok := os.LookupEnv("DEEPSEEK_API_KEY"); ok {
 		baseURL := os.Getenv("DEEPSEEK_BASE_URL")
 		if baseURL == "" {
@@ -59,5 +70,38 @@ func main() {
 
 		log.Fatal("environment variable DEEPSEEK_API_KEY is not set")
 	}
+}
 
+// ConfigRuntime sets the number of operating system threads.
+func ConfigRuntime() {
+	nuCPU := runtime.NumCPU()
+	runtime.GOMAXPROCS(nuCPU)
+	log.Printf("Running with %d CPUs\n", nuCPU)
+}
+
+// StartWorkers start starsWorker by goroutine.
+func StartWorkers() {
+	go statsWorker()
+}
+
+// StartGin starts gin web server with setting router.
+func StartGin() {
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.Default()
+	router.Use(rateLimit, gin.Recovery())
+	router.LoadHTMLGlob("resources/*.templ.html")
+	router.Static("/static", "resources/static")
+	router.GET("/", index)
+	router.GET("/room/:roomid", roomGET)
+	router.POST("/room-post/:roomid", roomPOST)
+	router.GET("/stream/:roomid", streamRoom)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	if err := router.Run(":" + port); err != nil {
+		log.Panicf("error: %s", err)
+	}
 }
