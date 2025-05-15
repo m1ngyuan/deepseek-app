f46dad402f2af57f3dc71b4e70ee8209964b3e6d
Mingyuan Wu <my.wu@outlook.com>
2025-05-15 16:11:37 +0800

    Support testing

---
+++ chat.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"github.com/openai/openai-go"
+	"github.com/openai/openai-go/option"
+	"log"
+	"os"
+)
+
+func chat() (string, error) {
+	if apiKey, ok := os.LookupEnv("DEEPSEEK_API_KEY"); ok {
+		baseURL := os.Getenv("DEEPSEEK_BASE_URL")
+		if baseURL == "" {
+			baseURL = "https://api.deepseek.com"
+		}
+		client := openai.NewClient(
+			option.WithAPIKey(apiKey), // defaults to os.LookupEnv("OPENAI_API_KEY")
+			option.WithBaseURL(baseURL),
+		)
+		stream := client.Chat.Completions.NewStreaming(context.TODO(), openai.ChatCompletionNewParams{
+			Messages: []openai.ChatCompletionMessageParamUnion{
+				openai.UserMessage("你如何评价小米ultra"),
+			},
+			Model: "deepseek-reasoner",
+		})
+		// optionally, an accumulator helper can be used
+		acc := openai.ChatCompletionAccumulator{}
+
+		for stream.Next() {
+			chunk := stream.Current()
+			acc.AddChunk(chunk)
+
+			if content, ok := acc.JustFinishedContent(); ok {
+				log.Println("Content stream finished:", content)
+			}
+
+			// if using tool calls
+			if tool, ok := acc.JustFinishedToolCall(); ok {
+				log.Println("Tool call stream finished:", tool.Index, tool.Name, tool.Arguments)
+			}
+
+			if refusal, ok := acc.JustFinishedRefusal(); ok {
+				log.Println("Refusal stream finished:", refusal)
+			}
+
+			// it's best to use chunks after handling JustFinished events
+			if len(chunk.Choices) > 0 && len(chunk.Choices[0].Delta.Content) > 0 {
+				log.Println(chunk.Choices[0].Delta.Content)
+			}
+		}
+		if err := stream.Err(); err != nil {
+			log.Fatalf("Stream error: %v", err)
+		}
+		finalContent := acc.Choices[0].Message.Content
+
+		return finalContent, nil
+	} else {
+
+		log.Fatal("environment variable DEEPSEEK_API_KEY is not set")
+		return "", errors.New("environment variable DEEPSEEK_API_KEY is not set")
+	}
+
+}
